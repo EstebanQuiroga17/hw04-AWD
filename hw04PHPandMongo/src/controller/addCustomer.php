a91fa4159d081dc5db022d855e5f45331fbd943a
Esteban Quiroga <eaquiroga@espe.edu.ec>
2026-04-23 21:18:08 -0500

    index.php moved to root

--- hw04PHPandMongo/src/controller/addCustomer.php
+++ hw04PHPandMongo/src/controller/addCustomer.php
@@ -1,5 +1,5 @@
 <?php
-require_once __DIR__ . '/../../vendor/autoload.php';
+require_once __DIR__ . '/../../../vendor/autoload.php';
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $id = htmlspecialchars($_POST['id'] ?? '');
